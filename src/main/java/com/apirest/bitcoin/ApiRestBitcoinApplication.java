78a68226a629783e21ec280b4cc59e3617690a3e
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-12-28 14:33:41 -0300

    [development] - BlockHound para não bloquear nossas requisições

--- src/main/java/com/apirest/bitcoin/ApiRestBitcoinApplication.java
+++ src/main/java/com/apirest/bitcoin/ApiRestBitcoinApplication.java
@@ -2,10 +2,13 @@ package com.apirest.bitcoin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class ApiRestBitcoinApplication {
-
+    static {
+        BlockHound.install();
+    }
     public static void main(String[] args) {
         SpringApplication.run(ApiRestBitcoinApplication.class, args);
     }
