2f3e9ed9ffa8971ffc8c2496cc845ffba4725daf
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-12-28 14:13:33 -0300

    configurações iniciais de banco de dados

---
+++ src/main/java/com/apirest/bitcoin/ApiRestBitcoinApplication.java
@@ -0,0 +1,13 @@
+package com.apirest.bitcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestBitcoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestBitcoinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apirest/bitcoin/ApiRestBitcoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.bitcoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestBitcoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
