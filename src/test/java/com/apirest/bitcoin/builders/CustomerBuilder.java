8592b57f46d05d09321bff2d488d70d374289f65
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-12-29 17:09:25 -0300

    [development] - Correção de dependências

---
+++ src/test/java/com/apirest/bitcoin/builders/CustomerBuilder.java
@@ -0,0 +1,46 @@
+package com.apirest.bitcoin.builders;
+
+import com.apirest.bitcoin.domain.Customer;
+
+import java.math.BigDecimal;
+
+public class CustomerBuilder {
+    private Customer customer;
+
+    public static CustomerBuilder createCustomerToBeSaved(){
+        CustomerBuilder builder = new CustomerBuilder();
+        builder.customer = Customer.builder()
+                .name("Heuler")
+                .document("02205652281")
+                .balance(BigDecimal.valueOf(10000))
+                .build();
+        return builder;
+    }
+
+
+    public static CustomerBuilder createValidCustomer(){
+        CustomerBuilder builder = new CustomerBuilder();
+        builder.customer = Customer.builder()
+                .id(1L)
+                .name("Heuler")
+                .document("02205652281")
+                .balance(BigDecimal.valueOf(10000))
+                .build();
+        return builder;
+    }
+
+    public static CustomerBuilder createValidUpdateCustomer(){
+        CustomerBuilder builder = new CustomerBuilder();
+        builder.customer = Customer.builder()
+                .id(1L)
+                .name("Heuler")
+                .document("02205652281")
+                .balance(BigDecimal.valueOf(10000))
+                .build();
+        return builder;
+    }
+
+    public Customer build() {
+        return customer;
+    }
+}
