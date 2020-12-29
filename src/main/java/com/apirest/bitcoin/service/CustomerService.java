b601179c74cb5c5c6673a9d627e9ae0ac44f4eb1
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-12-29 17:17:16 -0300

    [development] - Método para salvar customer

--- src/main/java/com/apirest/bitcoin/service/CustomerService.java
+++ src/main/java/com/apirest/bitcoin/service/CustomerService.java
@@ -6,12 +6,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
+
+    public Mono<Customer> save(Customer customer){
+        return customerRepository.save(customer);
+    }
 
     public Flux<Customer> findAll(){
         return customerRepository.findAll();
