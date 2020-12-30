599c16e62fcb124500d174b201e783aae5b3e8d4
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-12-30 11:05:04 -0300

    [development] - endpoints CRUD para Customer

--- src/main/java/com/apirest/bitcoin/controller/CustomerController.java
+++ src/main/java/com/apirest/bitcoin/controller/CustomerController.java
@@ -4,10 +4,11 @@ import com.apirest.bitcoin.domain.Customer;
 import com.apirest.bitcoin.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
 
 @RestController
 @Slf4j
@@ -21,5 +22,25 @@ public class CustomerController {
     @GetMapping()
     public Flux<Customer> findAll() {
         return customerService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Customer> findById(@PathVariable Long id) {
+        return customerService.findById(id);
+    }
+
+    @PostMapping
+    public Mono<Customer> save(@Valid @RequestBody Customer customer) {
+        return customerService.save(customer);
+    }
+
+    @PutMapping("/{id}")
+    public Mono<Customer> update(@PathVariable Long id, @Valid @RequestBody Customer customer) {
+        return customerService.update(customer.withId(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> delete(@PathVariable Long id) {
+        return customerService.delete(id);
     }
 }
