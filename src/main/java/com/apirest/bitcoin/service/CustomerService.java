7e99c61d8631915529d79a60c3378d72d9bd91fe
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2021-01-08 20:50:23 -0300

    [development] - regra para verificar cpf válido ao salvar e verificar se cpf já existe

--- src/main/java/com/apirest/bitcoin/service/CustomerService.java
+++ src/main/java/com/apirest/bitcoin/service/CustomerService.java
@@ -3,6 +3,7 @@ package com.apirest.bitcoin.service;
 import com.apirest.bitcoin.domain.Customer;
 import com.apirest.bitcoin.exception.MessageException;
 import com.apirest.bitcoin.repository.CustomerRepository;
+import com.apirest.bitcoin.validation.DocumentValidation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -17,9 +18,13 @@ import reactor.core.publisher.Mono;
 public class CustomerService {
     private final CustomerRepository customerRepository;
 
-    public Mono<Customer> save(Customer customer) throws MessageException {
-        return customerRepository.findByDocument(customer.getDocument())
-                .flatMap(existingDocument -> Mono.error(new MessageException("Cpf já existe")))
+
+    public Mono<Customer> save(Customer customer) {
+        return Mono.just(customer)
+                .filterWhen(cust -> isDocumentIsValid(cust.getDocument()).map(exist -> exist))
+                .switchIfEmpty(Mono.error(new MessageException("CPF is not valid")))
+                .flatMap(custToExisting -> customerRepository.findByDocument(customer.getDocument()))
+                .flatMap(existingDocument -> Mono.error(new MessageException("CPF Already Exists")))
                 .then(customerRepository.save(customer));
     }
 
@@ -32,12 +37,9 @@ public class CustomerService {
                 .switchIfEmpty(monoResponseStatusNotFoundException());
     }
 
-    private <T> Mono<T> monoResponseStatusNotFoundException() {
-        return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found"));
-    }
-
     public Mono<Customer> update(Customer customer) {
-        return findById(customer.getId())
+        return customerRepository.findById(customer.getId())
+                .switchIfEmpty(Mono.error(new MessageException("Custumer not found")))
                 .flatMap(cust -> {
                     cust.setBalanceReal(customer.getBalanceReal());
                     return customerRepository.save(cust);
@@ -46,8 +48,17 @@ public class CustomerService {
     }
 
     public Mono<Void> delete(String customerId) {
-        return findById(customerId)
+        return customerRepository.findById(customerId)
+                .switchIfEmpty(Mono.error(new MessageException("Custumer not found")))
                 .flatMap(customerRepository::delete);
+    }
+
+    private <T> Mono<T> monoResponseStatusNotFoundException() {
+        return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found"));
+    }
+
+    private Mono<Boolean> isDocumentIsValid(String document) {
+        return Mono.just(DocumentValidation.cpfValido(document));
     }
 
 }
