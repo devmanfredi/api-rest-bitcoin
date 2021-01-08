b86a12d82edd26a0d5eea4b2ae7caa92bb402fee
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2021-01-08 20:55:18 -0300

    [development] - Ajuste de classe

--- src/main/java/com/apirest/bitcoin/domain/Customer.java
+++ src/main/java/com/apirest/bitcoin/domain/Customer.java
@@ -2,31 +2,29 @@ package com.apirest.bitcoin.domain;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
+@Document
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @With
-@Table("customer")
 public class Customer {
 
     @Id
-    private Long id;
+    private String id;
 
     @NotNull
-    @NotEmpty(message = "The name cannot be empty")
     private String name;
 
     @NotNull
-    @NotEmpty(message = "The document cannot be empty")
     private String document;
 
-    @NotNull
-    private BigDecimal balance;
+    private BigDecimal balanceReal;
+
+    private BigDecimal balanceBitcoin;
 }
