12c32e802a5275da4cbaafbdbea5dded32c13e7a
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2021-01-10 08:16:47 -0300

    [development] - configuração mongo

--- src/main/java/com/apirest/bitcoin/ApiRestBitcoinApplication.java
+++ src/main/java/com/apirest/bitcoin/ApiRestBitcoinApplication.java
@@ -1,16 +1,40 @@
 package com.apirest.bitcoin;
 
+import com.apirest.bitcoin.domain.Customer;
+import com.apirest.bitcoin.repository.CustomerRepository;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactor.blockhound.BlockHound;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "APIs", version = "1.0", description = "Documentation APIs v1.0"))
 public class ApiRestBitcoinApplication {
-    static {
-        BlockHound.install();
-    }
+
     public static void main(String[] args) {
         SpringApplication.run(ApiRestBitcoinApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(ReactiveMongoOperations operations,
+                           CustomerRepository customerRepository) {
+        return args -> {
+            Flux<Customer> customerFlux = Flux.just(
+                    new Customer(null, "Heuler Manfredi", "02205652281", null, null),
+                    new Customer(null, "customer2", "02205652282", null, null),
+                    new Customer(null, "customer3", "02205652283", null, null),
+                    new Customer(null, "customer4", "02205652284", null, null))
+                    .flatMap(customerRepository::save);
+
+            customerFlux
+                    .thenMany(customerRepository.findAll())
+                    .subscribe(System.out::println);
+
+        };
     }
 
 }
