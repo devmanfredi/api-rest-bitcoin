84b69d4cc017a54d058ed2eb1f2a29e36311e3c5
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2021-01-11 07:17:13 -0300

    [development] - classe para persistencia e regras do extrato

---
+++ src/main/java/com/apirest/bitcoin/service/ExtractService.java
@@ -0,0 +1,18 @@
+package com.apirest.bitcoin.service;
+
+import com.apirest.bitcoin.domain.BitcoinResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ExtractService {
+
+    public Mono<Void> save(BigDecimal quantity, Mono<BitcoinResponse> price) {
+    }
+}
